87cbbbc9a3e1d59a218f3edb5494a017160c7186
Dmitry <zimokk1@gmail.com>
2016-10-23 14:55:22 +0200

    Edited registratio form & controller

--- src/client/app/signup/signup.component.ts
+++ src/client/app/signup/signup.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import {NgForm} from '@angular/forms';
 
-/**
-*	This class represents the lazy loaded SignupComponent.
-*/
+import { UserService } from '../servicies/user.service'
 
 @Component({
 	moduleId: module.id,
 	selector: 'signup-cmp',
-	templateUrl: 'signup.component.html'
+	templateUrl: 'signup.component.html',
+  providers: [UserService]
 })
 
-export class SignupComponent { }
+export class SignupComponent {
+  constructor(private userService: UserService){
+  }
+
+  onSubmit(f: NgForm){
+    console.dir(this.login);
+  }
+
+  register(event): void{
+    console.dir(this);
+    debugger;
+    if(password == repeatedPassword){
+      return this.userService.register(login, password);
+    }
+  }
+}
--- src/client/app/signup/signup.module.ts
+++ src/client/app/signup/signup.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
 import { SignupComponent } from './signup.component';
 
 @NgModule({
-    imports: [CommonModule, RouterModule],
+    imports: [CommonModule, RouterModule, BrowserModule, FormsModule],
     declarations: [SignupComponent],
     exports: [SignupComponent]
 })
