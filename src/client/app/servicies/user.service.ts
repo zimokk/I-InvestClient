6650e2d3fcc4b3891fb26318294cebb2df0b6089
Dmitry <zimokk1@gmail.com>
2016-10-27 15:34:35 +0200

    Added user CRUD operations to the service

--- src/client/app/servicies/user.service.ts
+++ src/client/app/servicies/user.service.ts
@@ -1,8 +1,120 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers} from '@angular/http';
 
 @Injectable()
 export class UserService {
-  register(login, password): boolean {
-    return true;
+  private urlPrefix = "/user";
+  private baseUrl = "http://localhost:8000";
+
+  constructor(private http: Http){}
+
+  register(user) {
+    return this.http.post(
+      this.baseUrl+this.urlPrefix+'/new',
+      JSON.stringify(user),
+      {headers: new Headers({'Content-Type': 'application/json'})})
+    .toPromise()
+    .then(function (result) {
+      if(result.json().statusCode == 0){
+        return {
+          result: result.json().data,
+          statusCode: 0
+        }
+      } else{
+        return{
+          statusCode: 500,
+          err: result.json().data,
+          message: result.json().message
+        }
+      }
+    });
+  }
+
+  get(id: string){
+    return this.http.get(
+      this.baseUrl+this.urlPrefix+'/get/'+id,
+      JSON.stringify({}),
+      {headers: new Headers({'Content-Type': 'application/json'})})
+    .toPromise()
+    .then(function (result) {
+      if(result.json().statusCode == 0){
+        return {
+          result: result.json().data,
+          statusCode: 0
+        }
+      } else{
+        return{
+          statusCode: 500,
+          err: result.json().data,
+          message: result.json().message
+        }
+      }
+    });
+  }
+
+  getAll(){
+    return this.http.get(
+      this.baseUrl+this.urlPrefix+'/all',
+      JSON.stringify({}),
+      {headers: new Headers({'Content-Type': 'application/json'})})
+    .toPromise()
+    .then(function (result) {
+      if(result.json().statusCode == 0){
+        return {
+          result: result.json().data,
+          statusCode: 0
+        }
+      } else{
+        return{
+          statusCode: 500,
+          err: result.json().data,
+          message: result.json().message
+        }
+      }
+    });
+  }
+
+  update(user){
+    return this.http.put(
+      this.baseUrl+this.urlPrefix+'/update/'+id,
+      JSON.stringify(user),
+      {headers: new Headers({'Content-Type': 'application/json'})})
+    .toPromise()
+    .then(function (result) {
+      if(result.json().statusCode == 0){
+        return {
+          result: result.json().data,
+          statusCode: 0
+        }
+      } else{
+        return{
+          statusCode: 500,
+          err: result.json().data,
+          message: result.json().message
+        }
+      }
+    });
+  }
+
+  remove(id: string){
+    return this.http.delete(
+      this.baseUrl+this.urlPrefix+'/delete/'+id,
+      JSON.stringify({}),
+      {headers: new Headers({'Content-Type': 'application/json'})})
+    .toPromise()
+    .then(function (result) {
+      if(result.json().statusCode == 0){
+        return {
+          result: result.json().data,
+          statusCode: 0
+        }
+      } else{
+        return{
+          statusCode: 500,
+          err: result.json().data,
+          message: result.json().message
+        }
+      }
+    });
   }
 }
