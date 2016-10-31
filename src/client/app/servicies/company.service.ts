f562c1dea6d763e483156b96a680ff6a5a6584c4
Dmitry <zimokk1@gmail.com>
2016-10-31 11:32:29 +0200

    Added companies service

---
+++ src/client/app/servicies/company.service.ts
@@ -0,0 +1,120 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers} from '@angular/http';
+
+@Injectable()
+export class CompaniesService {
+  private urlPrefix = "/company";
+  private baseUrl = "http://localhost:8000";
+
+  constructor(private http: Http){}
+
+  addCompany(company) {
+    return this.http.post(
+      this.baseUrl+this.urlPrefix+'/new',
+      JSON.stringify({company: company}),
+      {headers: new Headers({'Content-Type': 'application/json'})})
+      .toPromise()
+      .then(function (result) {
+        if(result.json().statusCode == 0){
+          return {
+            result: result.json().data,
+            statusCode: 0
+          }
+        } else{
+          return{
+            statusCode: 500,
+            err: result.json().data,
+            message: result.json().message
+          }
+        }
+      });
+  }
+
+  get(id: string){
+    return this.http.get(
+      this.baseUrl+this.urlPrefix+'/get/'+id,
+      {},
+      {headers: new Headers({'Content-Type': 'application/json'})})
+      .toPromise()
+      .then(function (result) {
+        if(result.json().statusCode == 0){
+          return {
+            data: result.json().data,
+            statusCode: 0
+          }
+        } else{
+          return{
+            statusCode: 500,
+            err: result.json().data,
+            message: result.json().message
+          }
+        }
+      });
+  }
+
+  getAll(){
+    return this.http.get(
+      this.baseUrl+this.urlPrefix+'/all',
+      {}
+    )
+      .toPromise()
+      .then(function (result) {
+        if(result.json().statusCode == 0){
+          return {
+            result: result.json().data,
+            statusCode: 0
+          }
+        } else{
+          return{
+            statusCode: 500,
+            err: result.json().data,
+            message: result.json().message
+          }
+        }
+      });
+  }
+
+  update(company){
+    return this.http.put(
+      this.baseUrl+this.urlPrefix+'/update/'+id,
+      JSON.stringify({company:company}),
+      {headers: new Headers({'Content-Type': 'application/json'})})
+      .toPromise()
+      .then(function (result) {
+        if(result.json().statusCode == 0){
+          return {
+            result: result.json().data,
+            statusCode: 0
+          }
+        } else{
+          return{
+            statusCode: 500,
+            err: result.json().data,
+            message: result.json().message
+          }
+        }
+      });
+  }
+
+  remove(id: string){
+    return this.http.delete(
+      this.baseUrl+this.urlPrefix+'/delete/'+id,
+      {}
+    )
+      .toPromise()
+      .then(function (result) {
+        if(result.json().statusCode == 0){
+          return {
+            result: result.json().data,
+            statusCode: 0
+          }
+        } else{
+          return{
+            statusCode: 500,
+            err: result.json().data,
+            message: result.json().message
+          }
+        }
+      });
+  }
+}
