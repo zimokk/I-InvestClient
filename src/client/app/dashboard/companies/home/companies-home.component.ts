c271dd76961270d9e4b223ee6ad2457245a4150f
Dmitry <zimokk1@gmail.com>
2016-10-31 11:32:18 +0200

    Added all companies table

--- src/client/app/dashboard/companies/home/companies-home.component.ts
+++ src/client/app/dashboard/companies/home/companies-home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {NotificationsService} from "angular2-notifications/components";
+import {CompaniesService} from "../../../servicies/company.service";
 
 @Component({
   moduleId: module.id,
@@ -8,12 +9,32 @@ import {NotificationsService} from "angular2-notifications/components";
 })
 
 export class CompaniesHomeComponent {
-  public users = [];
+  public companies = [];
+  public isLoading = true;
 
-  constructor(private  notificationService: NotificationsService) {
-    console.log("yeap");
+  constructor(private  notificationService: NotificationsService, private companiesService: CompaniesService) {
   }
 
   ngOnInit(){
+    this.getCompanies()
+  }
+
+  public getCompanies():void{
+    let self = this;
+    this.companiesService.getAll().then(function (result) {
+      if(result.statusCode == 0){
+        self.companies = result.result;
+      } else {
+        self.notificationService.error("Error", "An error quering users list");
+      }
+      self.toggleLoader();
+    })
+  }
+
+  private toggleLoader(): void{
+    let self = this;
+    setTimeout(function () {
+      self.isLoading = !self.isLoading;
+    }, 500)
   }
 }
