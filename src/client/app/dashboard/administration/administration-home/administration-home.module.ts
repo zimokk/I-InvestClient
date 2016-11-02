5a68553f60da0164067d59d1a106c43a51f017d0
Dmitry <zimokk1@gmail.com>
2016-11-02 08:24:08 +0200

    Changed pagination to bootstrap tmpl

--- src/client/app/dashboard/administration/administration-home/administration-home.module.ts
+++ src/client/app/dashboard/administration/administration-home/administration-home.module.ts
@@ -5,9 +5,12 @@ import { CommonModule } from '@angular/common'
 import {AdministrationHomeComponent} from "./administration-home.component";
 import {LoaderModule} from "../../../shared/loader/loader.module";
 import {Ng2PaginationModule} from "ng2-pagination/index";
+import {
+  PaginationModule
+} from 'ng2-bootstrap/ng2-bootstrap';
 
 @NgModule({
-  imports: [CommonModule, LoaderModule, RouterModule, Ng2PaginationModule, FormsModule],
+  imports: [CommonModule, LoaderModule, RouterModule, Ng2PaginationModule, FormsModule, PaginationModule],
   declarations: [AdministrationHomeComponent],
   exports: [AdministrationHomeComponent]
 })
