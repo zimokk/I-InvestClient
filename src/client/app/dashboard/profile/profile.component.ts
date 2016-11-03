5eae0443efdced8714506d9d529f03ebf7ef001d
Dmitry <zimokk1@gmail.com>
2016-11-03 18:37:57 +0200

    Fixed profile page view

--- src/client/app/dashboard/profile/profile.component.ts
+++ src/client/app/dashboard/profile/profile.component.ts
@@ -1,11 +1,16 @@
 import {Component} from '@angular/core';
 import {LoaderComponent} from "../../shared/loader/loader.component";
+import { NDV_DIRECTIVES } from 'angular2-click-to-edit/components';
 
 @Component({
   moduleId: module.id,
   selector: 'profile-cmp',
   templateUrl: 'profile.component.html',
-  directives: [LoaderComponent]
+  directives: [LoaderComponent, NDV_DIRECTIVES]
 })
 
-export class ProfileComponent {}
+export class ProfileComponent {
+  saveChanges(event){
+    console.dir(event);
+  }
+}
