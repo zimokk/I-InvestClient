847af1d5f8281c559841a3d7836ae59a2b5a55b7
Dmitry <zimokk1@gmail.com>
2016-11-10 12:58:35 +0200

    Added new company ation & view

--- src/client/app/dashboard/companies/new/company-new.component.ts
+++ src/client/app/dashboard/companies/new/company-new.component.ts
@@ -3,6 +3,8 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 import {NotificationsService} from "angular2-notifications/components";
 import {LoaderComponent} from "../../../shared/loader/loader.component";
 import {CompaniesService} from "../../../servicies/company.service";
+import {UserService} from "../../../servicies/user.service";
+import {AuthService} from "../../../servicies/auth.service";
 
 @Component({
   moduleId: module.id,
@@ -12,18 +14,43 @@ import {CompaniesService} from "../../../servicies/company.service";
 })
 
 export class CompanyNewComponent {
-  public currentCompany = {};
+  public currentCompany = {
+  };
   public isLoading = false;
+  private currentUser;
 
   constructor(
-    private  notificationService: NotificationsService,
+    private notificationService: NotificationsService,
     private route: ActivatedRoute,
     private router: Router,
-    private companiesService: CompaniesService) {
+    private companiesService: CompaniesService,
+    private userService: UserService,
+    private authService: AuthService) {
+  }
+
+  ngOnInit(){
+    let self = this;
+    self.toggleLoader();
+    self.userService.getByLogin(this.authService.getCurrentUser()).then(
+      result => {
+        self.currentUser = result.data;
+        self.toggleLoader();
+      }
+    );
   }
 
   onSubmit(){
-
+    let self = this;
+    self.toggleLoader();
+    if(self.currentUser){
+      self.currentCompany.userId = self.currentUser._id
+    }
+    self.companiesService.addCompany(self.currentCompany).then(function (result) {
+      if(result.statusCode == 0){
+        self.router.navigate(['companies']);
+        self.notificationService.success("Success","New company created");
+      }
+    })
   }
 
   private toggleLoader(): void{
--- src/client/app/dashboard/companies/new/company-new.module.ts
+++ src/client/app/dashboard/companies/new/company-new.module.ts
@@ -3,9 +3,10 @@ import { RouterModule } from '@angular/router'
 import { CommonModule } from '@angular/common';
 import {LoaderModule} from "../../../shared/loader/loader.module";
 import {CompanyNewComponent} from "./company-new.component";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, LoaderModule, RouterModule],
+  imports: [CommonModule, LoaderModule, RouterModule, FormsModule],
   declarations: [CompanyNewComponent],
   exports: [CompanyNewComponent, RouterModule],
   providers: []
