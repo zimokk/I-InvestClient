35ae69ae2d0ace50dc98c77b37398090a0fb63f6
Dmitry <zimokk1@gmail.com>
2016-11-15 18:26:37 +0200

    Added bounce-loader for small containers

---
+++ src/client/app/shared/loader/bounse-loader/bounce-loader.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'bounce-loader',
+  templateUrl: 'bounce-loader.html'
+})
+
+export class BounceLoaderComponent {
+
+}
--- src/client/app/shared/loader/loader.module.ts
+++ src/client/app/shared/loader/loader.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {LoaderComponent} from "./loader.component";
+import {BounceLoaderComponent} from "./bounse-loader/bounce-loader.component";
 
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LoaderComponent],
-  exports: [LoaderComponent],
+  declarations: [LoaderComponent, BounceLoaderComponent],
+  exports: [LoaderComponent,BounceLoaderComponent],
   providers: []
 })
 
