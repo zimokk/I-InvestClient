95ec79627d6f6b9ea3943f06501a8ad77e83f0d8
Dmitry <zimokk1@gmail.com>
2016-12-09 01:14:44 +0200

    Added company chart & data (fake

--- src/client/app/dashboard/companies/company/company.component.ts
+++ src/client/app/dashboard/companies/company/company.component.ts
@@ -3,17 +3,106 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 import {NotificationsService} from "angular2-notifications/components";
 import {LoaderComponent} from "../../../shared/loader/loader.component";
 import {CompaniesService} from "../../../servicies/company.service";
+import {nvD3} from "ng2-nvd3/build/lib/ng2-nvd3";
+import {nv} from 'nv';
 
 @Component({
   moduleId: module.id,
   selector: 'company-cmp',
   templateUrl: 'company.component.html',
-  directives: [LoaderComponent]
+  directives: [LoaderComponent, nvD3, nv]
 })
 
 export class CompanyComponent {
   public currentCompany = {};
   public isLoading = true;
+
+  public options = {
+    chart: {
+      type: 'pieChart',
+      height: 500,
+      x: function(d){return d.key;},
+      y: function(d){return d.y;},
+      showLabels: true,
+      duration: 500,
+      labelThreshold: 0.01,
+      labelSunbeamLayout: true,
+      legend: {
+        margin: {
+          top: 5,
+          right: 35,
+          bottom: 5,
+          left: 0
+        }
+      }
+    }
+  };
+  public data = [
+    {
+      key: "Opel",
+      y: 5
+    },
+    {
+      key: "Logitech",
+      y: 15
+    },
+    {
+      key: "WS",
+      y: 25
+    },
+    {
+      key: "Nokia",
+      y: 15
+    },
+    {
+      key: "Apple",
+      y: 10
+    },
+    {
+      key: "Jet Brains",
+      y: 28
+    },
+    {
+      key: "High definit",
+      y: 1
+    }
+  ];
+  public actions = [
+    {
+      "_id": "58297da557d2c422305fcabb",
+      "name": "Apple",
+      "change": 0.25
+    },
+    {
+      "_id": "582c2081dba9091d80a1aa32",
+      "name": "Nokia",
+      "change": 4
+    },
+    {
+      "_id": "582c2091dba9091d80a1b01b",
+      "name": "JetBrains",
+      "change": 12
+    },
+    {
+      "_id": "582c209fdba9091d80a1b604",
+      "name": "Opel",
+      "change": -5
+    },
+    {
+      "_id": "582c20acdba9091d80a1bbed",
+      "name": "WS",
+      "change": 11
+    },
+    {
+      "_id": "582c20d9dba9091d80a1c1d6",
+      "name": "Logitech",
+      "change": -25
+    },
+    {
+      "_id": "582c20e7dba9091d80a1c7bf",
+      "name": "High definit",
+      "change": 3
+    }];
 
   constructor(
     private  notificationService: NotificationsService,
--- src/client/app/dashboard/companies/company/company.module.ts
+++ src/client/app/dashboard/companies/company/company.module.ts
@@ -3,11 +3,12 @@ import { RouterModule } from '@angular/router'
 import { CommonModule } from '@angular/common';
 import {LoaderModule} from "../../../shared/loader/loader.module";
 import {CompanyComponent} from "./company.component";
+import {SharedModule} from "../../../shared/shared.module";
 
 @NgModule({
-  imports: [CommonModule, LoaderModule, RouterModule],
+  imports: [CommonModule, LoaderModule, RouterModule, SharedModule],
   declarations: [CompanyComponent],
-  exports: [CompanyComponent, RouterModule],
+  exports: [CompanyComponent, RouterModule, SharedModule],
   providers: []
 })
 
